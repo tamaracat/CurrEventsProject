475f83126a0b4dc3289877282f5547bc1b5b82a2
Tamara Tangney <tamaratangney@dhcp-10-102-152-33.wireless.northwestern.private>
2017-06-21 18:23:00 -0500

    add

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+var topics = ["apple marketplace"]
+
+function displayNewsInfo() {
+
+    console.log("in function");
+
+  var topic = $(this).attr("data-name");
+
+  console.log("topic");
+
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+		url += '?' + $.param({'api-key': "25e343624beb4f808ff678bfbcec222c",'q': topic,'begin_date': "20170501"
+		});
+      
+    $.ajax({
+  		url: url,
+  		method: 'GET',
+		}).done(function(result) {
+  		console.log(result);
+		}).fail(function(err) {
+  		throw err;
+		});
+        console.log(url);
+        // Creates AJAX call for the specific movie button being clicke
+  }
+$(document).on("click", ".topic-input", displayNewsInfo);
