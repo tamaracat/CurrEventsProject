947001e6fee51f669d24a695d2966cee4dc9a0ac
Alex <Alex-Carver@.noreply.github.com>
2017-06-21 19:22:48 -0500

    Css File

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,19 @@
-var topics = ["apple marketplace"]
 
-function displayNewsInfo() {
+//On click events to display articles 
 
-    console.log("in function");
+$("#show1").click(function(){
+    $("#articles1").show();
+});
 
-  var topic = $(this).attr("data-name");
+$("#show2").click(function(){
+    $("#articles2").show();
+    $("#articles1").hide();
+});
 
-  console.log("topic");
+$("#show3").click(function(){
+    $("#articles3").show();
+    $("#articles2").hide();
+    $("#articles1").hide();
+});
 
-  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-		url += '?' + $.param({'api-key': "25e343624beb4f808ff678bfbcec222c",'q': topic,'begin_date': "20170501"
-		});
-      
-    $.ajax({
-  		url: url,
-  		method: 'GET',
-		}).done(function(result) {
-  		console.log(result);
-		}).fail(function(err) {
-  		throw err;
-		});
-        console.log(url);
-        // Creates AJAX call for the specific movie button being clicke
-  }
-$(document).on("click", ".topic-input", displayNewsInfo);
+
