a84ba3ba5db0584d61cf7a4808fbadaf7d1ac11d
Douglas Balkin <dbalkin77@gmail.com>
2017-06-22 23:27:58 -0500

    ajax call to news API created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,11 @@
+// Variables ////////////////////////////////////////////////////////////////////////
+
+var allSources = ["abc-news-au", "al-jazeera-english","bbc-news", "bloomberg", "cnbc", "cnn", "google-news", "breitbart-news", "daily-mail", "reuters", "the-guardian-uk", "the-new-york-times", "the-wall-street-journal", "time", "the-washington-post"];
+var randomSource = Math.floor(Math.random() * allSources.length);
+var selectedSource = allSources[randomSource];
+
+
+// Functions ////////////////////////////////////////////////////////////////////////
 
 function displayNewsInfo() {
 
@@ -23,3 +31,27 @@ function displayNewsInfo() {
         // Creates AJAX call for the specific movie button being clicke
   }
 $(document).on("click", ".add-topic", displayNewsInfo);
+
+// Function to select random news source
+
+// Function to generate 10 images to DOM
+
+function displayTenArticles () {
+  var article = $(this).attr("data-name");
+  var queryURL = "https://newsapi.org/v1/articles?source=" + selectedSource + "&sortBy=top&apiKey=01aed6729dc84b87b67d8eca2e2a711b"
+
+  $.ajax({
+    url:queryURL,
+    method: 'GET',
+  }).done(function(response) {
+    console.log(response);
+
+  });
+}
+
+// Main Process ////////////////////////////////////////////////////////////////////////
+displayTenArticles();
+console.log(allSources);
+console.log(randomSource);
+console.log(selectedSource);
+
